5e7d258dc12d636d48b8b92ac2e4713873e09222
BroadSkyC <1115896263@qq.com>
2022-05-08 20:51:39 +0800

    dev2

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,28 @@
+import java.util.Calendar;
+
 public class Person {
     private String name;
+
+    private ITCompany workPlace;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+
+    public void sayHello(String hello){
+        System.out.println(hello);
+    }
+
+    public int getAge(){
+        Calendar calendar = Calendar.getInstance();
+        return calendar.get(Calendar.YEAR) - 2000;
+    }
 }
