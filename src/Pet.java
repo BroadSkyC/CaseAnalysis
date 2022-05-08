1fb615ee41da916ab797770dfb1760504684aa88
BroadSkyC <1115896263@qq.com>
2022-05-08 17:18:48 +0800

    allow get pet's name

--- src/Pet.java
+++ src/Pet.java
@@ -1,3 +1,11 @@
 public class Pet extends Animal{
     private String name;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getName(String masterName){
+        return masterName + "的" + this.name;
+    }
 }
