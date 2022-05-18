8bc22462a7a9986c7675e90041b532f2c3629567
BroadSkyC <1115896263@qq.com>
2022-05-17 18:47:21 -0700

    update

--- curr/Controller.java
+++ curr/Controller.java
@@ -1,3 +1,5 @@
+package curr;
+
 import location.Hospital;
 import location.Infrastructure;
 import location.InfrastructureType;
--- prev/Controller.java
+++ prev/Controller.java
@@ -1,3 +1,5 @@
+package prev;
+
 import location.FoundProperty;
 import location.Infrastructure;
 
